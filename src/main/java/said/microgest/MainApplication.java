2effd5a9fbaf3a80c716533f8ba2d5d263cd87ab
FOUADI Said <saidfouadisaid@gmail.com>
2026-08-22 01:34:44 +0000

    fix: Resous de bug sur le hashage de password

--- src/main/java/said/microgest/MainApplication.java
+++ src/main/java/said/microgest/MainApplication.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.mindrot.jbcrypt.BCrypt;
 import said.microgest.config.HibernateUtil;
 
 public class MainApplication extends Application {
